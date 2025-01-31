0a01bfee31359c2e444b00006337bcc343b093af
Praveen Kukudala <praveenkukudala@Praveens-MacBook-Air-2.local>
2025-01-31 14:35:02 -0600

    updated1

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base :  '/AutoCare/'
+  base :  '/AutoCare/',
+  root: ".", // Ensure Vite looks in the correct directory
+  build: {
+    outDir: "dist",
+  },
 });
